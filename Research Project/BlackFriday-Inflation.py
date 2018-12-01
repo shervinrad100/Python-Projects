b9d43888cb60343c0b4b432d675dc2e6503ca36e
shervinrad100 <40181828+shervinrad100@users.noreply.github.com>
2018-12-01 01:41:28 +0000

    Add

--- Research Project/BlackFriday-Inflation.py
+++ Research Project/BlackFriday-Inflation.py
@@ -5,3 +5,47 @@ Created on Fri Nov 30 22:34:10 2018
 @author: sherv
 """
 
+import pandas as pd
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import data and construct moving average 
+# Disposable income from: https://fred.stlouisfed.org/series/DSPIC96
+
+disp_income = pd.read_csv("RealDisposableIncome-2004-1_Present-Mon-US(Grab-30-11-18).csv")
+trends_amazon = pd.read_csv("Amazon-2004-1_Present-mon-US(Grab-30-11-18).csv")
+
+# are the datasets of the same length?
+"""
+equal = len(disp_income)==len(trends_amazon)
+if equal == False:
+    print(disp_income)
+    print(trends_amazon)
+else:
+    pass
+"""
+# investigate the sets and parse accordingly
+trends_amazon = trends_amazon[:-2]
+dates = mdates.num2date(mdates.datestr2num(trends_amazon['Month']))
+
+# Check to see if the are the same
+"""
+time = mdates.num2date(mdates.datestr2num(disp_income['DATE']))
+time == dates
+"""
+amazon = trends_amazon['amazon: (United States)']
+RDPI = disp_income['DSPIC96']
+
+def MA(series, n): # https://www.youtube.com/watch?v=5-SV_xXQ_wE
+    weights = np.repeat(1.0, n)/n
+    MA = np.convolve(series,weights,'valid')
+    return MA
+
+RDPI_MA = MA(RDPI,12)
+
+plt.figure()
+plt.plot(dates,amazon)
+plt.figure()
+plt.plot(dates,RDPI)
+plt.plot(dates[:-11],RDPI_MA)
