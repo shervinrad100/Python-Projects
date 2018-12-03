1a2a775386ee035fd3c41035b9dba1a1bc9b1cfb
shervinrad100 <40181828+shervinrad100@users.noreply.github.com>
2018-12-03 00:54:45 +0000

    Fix - DSPIC/Amazon

--- Research Project/BlackFriday-Inflation.py
+++ Research Project/BlackFriday-Inflation.py
@@ -10,37 +10,25 @@ import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Import data and construct moving average 
 # Disposable income from: https://fred.stlouisfed.org/series/DSPIC96
 
-disp_income = pd.read_csv("RealDisposableIncome-2004-1_Present-Mon-US(Grab-30-11-18).csv")
-trends_amazon = pd.read_csv("Amazon-2004-1_Present-mon-US(Grab-30-11-18).csv")
+DSPIC96 = pd.read_csv("RealDisposableIncome-2004-1_Present-Mon-US(Grab-30-11-18).csv")
+Google_amazon_US = pd.read_csv("Amazon-2004-1_Present-mon-US(Grab-30-11-18).csv")
 
-# are the datasets of the same length?
-"""
-equal = len(disp_income)==len(trends_amazon)
-if equal == False:
-    print(disp_income)
-    print(trends_amazon)
-else:
-    pass
-"""
-# investigate the sets and parse accordingly
-trends_amazon = trends_amazon[:-2]
-dates = mdates.num2date(mdates.datestr2num(trends_amazon['Month']))
+Google_amazon_US = Google_amazon_US[:-2] # Google has more data so we delete it
+dates = mdates.num2date(mdates.datestr2num(DSPIC96['DATE']))
 
-# Check to see if the are the same
-"""
-time = mdates.num2date(mdates.datestr2num(disp_income['DATE']))
-time == dates
-"""
-amazon = trends_amazon['amazon: (United States)']
-RDPI = disp_income['DSPIC96']
+MA_time = mdates.num2date(mdates.datestr2num(Google_amazon_US['Month']))
+
+
+amazon = Google_amazon_US['amazon: (United States)']
+RDPI = DSPIC96['DSPIC96']
 
 def MA(series, n): # https://www.youtube.com/watch?v=5-SV_xXQ_wE
     weights = np.repeat(1.0, n)/n
     MA = np.convolve(series,weights,'valid')
     return MA
+
 n =12
 RDPI_MA = MA(RDPI,n)
 
@@ -48,4 +36,5 @@ plt.figure()
 plt.plot(dates,amazon)
 plt.figure()
 plt.plot(dates,RDPI)
-plt.plot(dates,RDPI_MA)
+plt.plot(RDPI_MA)
+
--- Research Project/BlackFriday-RDPI.py
+++ Research Project/BlackFriday-RDPI.py
@@ -1,7 +1,31 @@
-import numpy as np
-import matplotlib.dates as mdates
+#import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# Import data
+#PATH = r"C:\Users\sherv\OneDrive\Documents\GitHub\Python - Projects\Research Project\Data"
+Google_amazon_US = pd.read_csv("Amazon-2004-1_Present-mon-US(Grab-30-11-18).csv", index_col="Month", parse_dates=["Month"])
+DSPIC = pd.read_csv("RealDisposableIncome-2004-1_Present-Mon-US(Grab-30-11-18).csv", index_col="DATE", parse_dates=["DATE"])
 
-Google_amazon_US = np.genfromtxt("Amazon-2004-1_Present-mon-US(Grab-30-11-18).csv", delimiter=',')
-del(Google_amazon_US[0:])
-dates = mdates.num2date(mdates.datestr2num(Google_amazon_US[:][:0]))
+# Adjust datasets
+Google_amazon_US = Google_amazon_US[:-2]
+
+# Create Moving Average
+DSPIC['MA_12'] = DSPIC["DSPIC96"].rolling(12).mean()
+
+# Google trends: amazon: (United States)
+plt.figure(1)
+Google_amazon_US["amazon: (United States)"].plot()
+plt.xlabel("Year")
+plt.ylabel("%")
+plt.grid()
+plt.legend()
+
+# Real Disposable Personal Incom
+plt.figure(2)
+DSPIC["DSPIC96"].plot()
+DSPIC["MA_12"].plot()
+plt.xlabel("Year")
+plt.ylabel("$")
+plt.grid()
+plt.legend()
