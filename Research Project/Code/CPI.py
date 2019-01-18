a09b25f70f3202e9426711e59473c2402db738e6
shervinrad100 <40181828+shervinrad100@users.noreply.github.com>
2019-01-18 14:18:47 +0000

    data

--- Research Project/Code/CPI.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-CPI = pd.read_csv("", index_col="", parse_date="")
-
-CPI["MA_X"] = CPI[""].rolling(X).mean().
-
-infl = (CPI[i]-CPI[i-1])/CPI[i-1]*100
