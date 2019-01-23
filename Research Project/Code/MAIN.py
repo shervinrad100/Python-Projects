b93302f89704637a086f30158e1fe70e5beaeb24
shervinrad100 <40181828+shervinrad100@users.noreply.github.com>
2019-01-23 04:10:28 +0000

    Update MAIN.py

--- Research Project/Code/MAIN.py
+++ Research Project/Code/MAIN.py
@@ -30,54 +30,103 @@ def MA_forward(df_in, window):
 def MA_central(df_in, window):
         return df_in.rolling(window, center=True).mean()
 
-def Compare(df_1, yName1, df_2, yName2, title):
+def Compare(df_1, yName1, df_2, yName2, Title):
+    r_squared = data[key][meta[key][2]].corr(data["google"]["Top5"])
     fig, ax1 = plt.subplots()
     df_1.plot(ax=ax1,color='b').xaxis.set_minor_locator(minor_locator)
     ax1.set_xlabel('Date')
     ax1.set_ylabel(yName1, color='b')
     ax1.tick_params('y', colors='b')
     plt.grid()
+    plt.text("2005-01-01", 100, r"$r^2=$ "+"%.2f" %(r_squared))
     ax2 = ax1.twinx()
     df_2.plot(ax=ax2,color='r')
     ax2.set_ylabel(yName2, color='r')
     ax2.tick_params('y', colors='r')
-    plt.title(title)
+    plt.title(Title)
+    plt.savefig("%s\%s.png" %(PATH, Title))
 
-def plot_regressors(key, yName, Title):
-    plt.figure()
-    data[key][[meta[key][2], "MA_12", "MA_6", "MA_3"]].plot().xaxis.set_minor_locator(minor_locator)
+def plot_regressors(key, lag, Title, central="true"):
+    fig, axes = plt.subplots()
+    data[key][meta[key][2]].plot(ax=axes).xaxis.set_minor_locator(minor_locator)
+    if central == "both":
+        for i in lag:
+            data[key][["MA_%s" %(i), "MAc_%s" %(i)]].plot(ax = axes)
+    elif central == "false":
+        for i in lag:
+            data[key][["MA_%s" %(i)]].plot(ax = axes)
+    elif central == "true":
+        for i in lag:
+            data[key][["MAc_%s" %(i)]].plot(ax = axes)
     plt.xlabel("Year")
-    plt.ylabel(yName)
+    plt.ylabel(meta[key][2])
     plt.grid()
     plt.legend()
     plt.title(Title)
+    plt.savefig("%s\%s.png" %(PATH, Title))
     
-def plot_residuals(key, Title):
-    plt.figure()
-    data[key][["e(12)", "ec(12)"]].plot().xaxis.set_minor_locator(minor_locator)
+    
+def plot_residuals(key, lag, Title, central="both"):
+    std={}
+    mu ={}
+    for i in lag:
+        if central == "both":
+            std["ec%s" %(i)] = "%.2f" %(data[key]["ec(%s)" %(i)].std())
+            std["e%s" %(i)] = "%.2f" %(data[key]["e(%s)" %(i)].std())
+            mu["ec%s" %(i)] = "%.2f" %(data[key]["ec(%s)" %(i)].mean())
+            mu["e%s" %(i)] = "%.2f" %(data[key]["e(%s)" %(i)].mean())
+        elif central == "true":
+            std["ec%s" %(i)] = "%.2f" %(data[key]["ec(%s)" %(i)].std())
+            mu["ec%s" %(i)] = "%.2f" %(data[key]["ec(%s)" %(i)].mean())
+        elif central == "false":
+            std["e%s" %(i)] = "%.2f" %(data[key]["e(%s)" %(i)].std())
+            mu["e%s" %(i)] = "%.2f" %(data[key]["e(%s)" %(i)].mean())
+    fig, axes = plt.subplots()
+    if central == "both":
+        for i in lag:
+            data[key][["e(%s)" %(i), "ec(%s)" %(i)]].plot(ax = axes).xaxis.set_minor_locator(minor_locator)
+            yax = data[key][["e(%s)" %(i), "ec(%s)" %(i)]].max() 
+            scale = yax //6
+            plt.text("2019-02-01",yax - scale,r"$\sigma_{c}^2=$ "+std["ec%s" %(i)])
+            plt.text("2019-02-01",yax - 2*scale,r"$\mu_{c}^2=$ "+mu["ec%s" %(i)])
+            plt.text("2019-02-01",yax - 3*scale,r"$\sigma^2=$ "+std["e%s" %(i)])
+            plt.text("2019-02-01",yax - 4*scale,r"$\mu^2=$ "+mu["e%s" %(i)])
+    elif central == "false":
+        for i in lag:
+            data[key][["e(%s)" %(i)]].plot(ax = axes).xaxis.set_minor_locator(minor_locator)
+    elif central == "true":
+        for i in lag:
+            data[key][["ec(%s)" %(i)]].plot(ax = axes).xaxis.set_minor_locator(minor_locator)    
     plt.xlabel("Year")
     plt.grid()
     plt.legend()
     plt.title(Title)
+    plt.savefig("%s\%s.png" %(PATH, Title))
     
 def plot_ACF(key, array, Title):
     plt.figure()
     autocorrelation_plot(data[key][array].dropna())
     plt.title(Title)
+    plt.savefig("%s\%s.png" %(PATH, Title))
 
-def distribution(key, arrayName, Title):
-    plt.figure()
-    data[key][arrayName].hist()   
-    plt.title(Title)
+def distribution(key, array, Title):
+    for arrayName in array:
+        plt.figure()
+        data[key][arrayName].hist()   
+        plt.title(Title)
+        plt.savefig("%s\%s.png" %(PATH, Title))
     
 def IID_test(key, array):
     Data = data[key][array].dropna()
     alpha = 0.05
     stat, p = shapiro(Data)
     plt.figure()
-    probplot(data[key][array].dropna(), plot=plt, rvalue=True)
-    plt.title(key+" "+array)
-    print(key, "'%s'" %(array), '\n \t Test Statistics=%.3f, p=%.3f' % (stat, p))
+    probplot(Data, plot=plt, rvalue=True)
+    plt.grid()
+    Title = key+" "+array+" QQ-plot"
+    plt.title(Title)
+    plt.savefig("%s\%s.png" %(PATH, Title))
+    print(key, "'%s'" %(array), "\n \t Test Statistics=%.3f, p=%.3f" %(stat, p))
     if p >= alpha:
         print(" \t %s Normally distributed" %(key+" "+"'%s'" %(array)))
     else:
@@ -85,7 +134,7 @@ def IID_test(key, array):
 
 
 # Data
-#PATH = r"C:\Users\sherv\OneDrive\Documents\GitHub\Python - Projects\Research Project\Data"    
+PATH = r"C:\Users\sherv\OneDrive\Documents\GitHub\Python - Projects\Research Project\Plots"    
 meta = {# dataset: [path, Date_col, Value_col]
         "google":["multiTimeline.csv", "Month", "Top5"], 
         "RDPI":  ["RealDisposableIncome-2004-1_Present-Mon-US(Grab-30-11-18).csv", "DATE", "DSPIC96"], 
@@ -134,26 +183,27 @@ for key in meta.keys():
     data[key]["ec(12)"] = data[key][meta[key][2]] - data[key]["MAc_12"]
     data[key]["ec(6)"] = data[key][meta[key][2]] - data[key]["MAc_6"]
     data[key]["ec(3)"] = data[key][meta[key][2]] - data[key]["MAc_3"]
-    # normality test
-#    print(key)
-#    print("\t ec(3) skew %.3f" %(data[key]["ec(3)"].skew()))
-#    print("\t ec(3) kurtosis %.3f" %(data[key]["ec(3)"].kurtosis()))
-#    IID_test(key, "ec(3)")
-#    IID_test(key, "R")
 
 # Visualise
     # Overal trends
-#    Compare(data["google"][meta["google"][2]], "Google Trends (%)", data[key][meta[key][2]], "%s" %(key), "Google Trends v %s" %(meta[key][2]))
-#    print("Corr(google, %s): " %(key), data[key][meta[key][2]].corr(data["google"]["Top5"]))
+    Compare(data["google"][meta["google"][2]], "Google Trends (%)", data[key][meta[key][2]], key, "Google Trends v %s" %(key))
     # Index and regressor
-#    plot_regressors(key, meta[key][2], key)
+    plot_regressors(key, [12,3], "12 and 3 forward rolling window MA- %s" %(key), central="false")
+    plot_regressors(key, [3], "central and forward 3 rolling window MA- %s" %(key), central="both")
     # Autocorrelation
-#    plot_ACF(key, meta[key][2], "%s ACF" %(key))
-#    plot_ACF(key, "ec(3)", "residual ACF: %s" %(key))
-    plot_ACF(key, "R", "residual ACF: %s" %(key))
+    plot_ACF(key, meta[key][2], "%s ACF" %(key))
+    plot_ACF(key, "ec(3)", "ec(3) residual ACF- %s" %(key))
+    plot_ACF(key, "R", "Return ACF- %s" %(key))
     # residual plots
-#    plot_residuals(key, "%s residuals" %(key))
-#    distribution(key, "ec(12)", "ec(12) Residual distribution: %s" %(key))
-#    distribution(key, "R", "Returns destribution: %s" %(key))
+#    plot_residuals(key, [3],  "%s residuals e(3), ec(3)" %(key), central="both")
+#    plot_residuals(key, [12,3],  "%s residuals (central window) ec(3),ec(12)" %(key), central="true")
+    distribution(key, ["ec(3)"], "ec(%i) Residual distribution: %s" %(3,key))
+    distribution(key, ["R"], "Returns destribution: %s" %(key))
+    # normality test
+    print(key)
+    print("\t ec(3) skew %.3f" %(data[key]["ec(3)"].skew()))
+    print("\t ec(3) kurtosis %.3f" %(data[key]["ec(3)"].kurtosis()))
+    IID_test(key, "ec(3)")
+    IID_test(key, "R")
 
 
