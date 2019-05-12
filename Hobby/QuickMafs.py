23e10d7558fd2168a776ebcb65800e44c7adf52f
shervinrad100 <40181828+shervinrad100@users.noreply.github.com>
2019-05-12 18:06:09 +0100

    quickmafs

--- Hobby/QuickMafs.py
+++ Hobby/QuickMafs.py
@@ -1,77 +1,161 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 27 13:05:28 2018
-
-@author: sherv
-"""
-
 import numpy as np 
+import time
 
-print('Wlecome to the \'quick mafs\' practice game' )
+print('Welcome to the \'quick mafs\' practice game' )
+counter = 0
+score = 0
+
+def gennumber():
+    a = np.random.randint(0,999)
+    b = np.random.randint(0,999)
+    a2 = round(np.random.random(),2)
+    b2 = round(np.random.random(),2)
+    while b > a:    
+        b = np.random.randint(0,999)
+    global counter                                                              # is this right?
+    counter +=1    
+    startT = time.time()
+    ans = input('%.2f - %.2f =' %(a, b))
+    endT = time.time()
+    print(f"Time elapsed: {int(endT-startT)}s")
+    return a,b,a2,b2, ans
 
  # The game:
 while True:
     print('If you wish to quit, please enter: \' quit \'')
     print('For help, please enter: \' help \'')
+    ez = input("Easy (True), or hard (False)? ").upper()
+    EZ = ["TRUE" , "T" , "+"]
+    if ez in EZ:
+        ez = True
+    elif ez == "quit":                                                          # not working?
+        break
+    else:
+        ez = False
     math = input('Please choose from: +, -, *, / \n')
+
+# Subtraction
     if math == '-':
         a = np.random.randint(0,999)
-        b = np.random.randint(0,999) 
+        b = np.random.randint(0,999)
+        a2 = round(np.random.random(),2)
+        b2 = round(np.random.random(),2)
        # Only generate numbers that will give positive results
         while b > a:    
             b = np.random.randint(0,999)
-            if b < a:
-                pass
-            else: 
-                b = np.random.randint(0,999)
-        
-        ans = input('%f - %f =' %(a, b))
-        if ans == str(a-b):
-            print('Correct')
-        else:
-            print('Wrong')
-            print(a-b)
+        if ez == True:
+            startT = time.time()
+            ans = input('%.2f - %.2f =' %(a, b))
+            endT = time.time()
+            print(f"Time elapsed: {int(endT-startT)}s")
+            if ans == str(a-b):
+                print('Correct')
+            else:
+                print('Wrong')
+                print(a-b)
+        elif ez == False:
+            startT = time.time()
+            ans = input('%.2f - %.2f =' %(a+a2, b+b2))
+            endT = time.time()
+            print(f"Time elapsed: {int(endT-startT)}s")
+            if ans == str(round(a+a2-(b+b2),2)):
+                print('Correct')
+            else:
+                print('Wrong')
+                print(round(a+a2-(b+b2),2))
+
+# Summation    
     elif math == '+':
         a = np.random.randint(0,999)
         b = np.random.randint(0,999)
-        ans = input('%f + %f =' %(a, b))
-        if ans == str(a+b):
-            print('Correct')
-        else:
-            print('Wrong')
-            print(a+b)
+        a2 = round(np.random.random(),2)
+        b2 = round(np.random.random(),2)
+        if ez == True:
+            startT = time.time()
+            ans = input('%.2f + %.2f =' %(a, b))
+            endT = time.time()
+            print(f"Time elapsed: {int(endT-startT)}s")
+            if ans == str(a+b):
+                print('Correct')
+            else:
+                print('Wrong')
+                print(a+b)
+        elif ez == False:
+            startT = time.time()
+            ans = input('%.2f + %.2f =' %(a+a2, b+b2))
+            endT = time.time()
+            print(f"Time elapsed: {int(endT-startT)}s")
+            if ans == str(round(a+a2+b+b2,2)):
+                print('Correct')
+            else:
+                print('Wrong')
+                print(round((a+a2+b+b2),2))
+
+# Multiplication      
     elif math == '*':
-        a = round(np.random.randint(0,100))
-        b = round(np.random.randint(0,100))
-        ans = input('%f * %f =' %(a, b))
-        if ans == str(a*b):
-            print('Correct')
-        else:
-            print('Wrong')
-            print(a*b)
+        a= np.random.randint(2,100)
+        b= np.random.randint(2,100)
+        a2 = round(np.random.random(),2)
+        b2 = round(np.random.random(),2)
+        if ez == True:
+            startT = time.time()
+            ans = input('%.2f * %.2f =' %(a, b))
+            endT = time.time()
+            print(f"Time elapsed: {int(endT-startT)}s")
+            if ans == str(a*b):
+                print('Correct')
+            else:
+                print('Wrong')
+                print(a*b)
+        elif ez == False:
+            startT = time.time()
+            ans = input('%.2f * %.2f =' %(a+a2, b+b2))
+            endT = time.time()
+            print(f"Time elapsed: {int(endT-startT)}s")
+            if ans == str((a+a2)*(b+b2)):
+                print('Correct')
+            else:
+                print('Wrong')
+                print((a+a2)*(b+b2))
+
+# Division
     elif math == '/':
-        a = round(np.random.randint(2,100))
-        b = round(np.random.randint(2,100))
-        
+        a= np.random.randint(2,100)
+        b= np.random.randint(2,100)
+        a2= round(np.random.random(),2)
+        b2 = round(np.random.random(),2)
         while b > a:    
             b = np.random.randint(2,100)
-            if b < a:
-                pass
-            else: 
-                b = np.random.randint(2,100)
+        if ez == True:
+            startT = time.time()
+            ans = input('%.2f / %.2f =' %(a, b))
+            endT = time.time()
+            print(f"Time elapsed: {int(endT-startT)}s")
+            if ans == str(round((a)/(b)),2):
+                print('Correct')
+            else:
+                print('Wrong')
+                print("%.2f" %((a)/(b)))
+        elif ez == False:
+            startT = time.time()
+            ans = input('%.2f / %.2f =' %(a+a2, b+b2))
+            endT = time.time()
+            print(f"Time elapsed: {int(endT-startT)}s")
+            if ans == str((a+a2)/(b+b2)):
+                print('Correct')
+            else:
+                print('Wrong')
+                print("%.2f" %((a+a2)/(b+b2)))
         
-        ans = input('%f / %f =' %(a, b))
-        if ans == str(a/b):
-            print('Correct')
-        else:
-            print('Wrong')
-            print(a/b)
     elif math == 'quit':
         break
+    
     elif math == 'help':
         print('The aim of the game is for you to practice your mental maths skills.',
               'You can choose one of the four main operations to practice.',
               'Two random numbers will be generated and you will need to enter your answer.')
+        
     else:
         continue
+    
 print('Thanks for playing')
---
+++ Hobby/TESTQuickMafs.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 11 11:47:01 2019
+
+@author: sherv
+"""
+import numpy as np
+import time
+
+
+def gennumber(n1=100,n2=100, dec1=True, dec2=True):
+    a = np.random.randint(0,n1)
+    b = np.random.randint(0,n2)
+    a2 = round(np.random.random(),2)
+    b2 = round(np.random.random(),2)
+    if dec1 == True:
+        A = a+a2
+    else:
+        A = a
+    if dec2 == True:
+        B = b+b2
+    else:
+        B = b
+    return A,B
+
+
+def timed(A,B, op="*"):
+    startT = time.time()
+    if op == "/":
+        if A>B:
+            ans = input('%.2f / %.2f =' %(A, B))
+        else:
+            ans = input('%.2f / %.2f =' %(B, A))
+    elif op == "+":
+        ans = input('%.2f + %.2f =' %(A, B))
+    elif op == "-":
+        if A>B:
+            ans = input('%.2f - %.2f =' %(A, B))
+        else:
+            ans = input('%.2f - %.2f =' %(B, A))    
+    else:
+        ans = input('%.2f * %.2f =' %(A, B))
+    endT = time.time()
+    elapsed = int(endT-startT)
+    print(f"Time elapsed: {elapsed}s")
+    return ans, elapsed
+    
+
+def check(A,B, op, ans):
+    if op == "*":
+        if ans == str(round((A)*(B),2)):
+            print('Correct')
+            point = "c"
+        elif ans == "":
+            point = "b"
+        else:
+            print('Wrong')
+            print(round((A)*(B),2))
+            point = "w"
+    if op == "+":
+        if ans == str(round(A+B,2)):
+            print("Correct")
+            point="c"
+        elif ans == "":
+            point = "b"
+        else:
+            print("Wrong")
+            print(round(A+B,2))
+            point = "w"
+    if op == "/":
+        if A>B:
+            if ans == str(round(A/B,2)):
+                print("Correct")
+                point = "c"
+            elif ans == "":
+                point = "b"
+            else:
+                print("Wrong")
+                print(round(A/B,2))
+                point = "w"
+        else:
+            if ans == str(round(B/A,2)):
+                print("Correct")
+                point = "c"
+            elif ans == "":
+                point="b"
+            else:
+                print("Wrong")
+                print(round(B/A,2))
+                point = "w"
+    if op == "-":
+        if A>B:
+            if ans == str(round(A-B,2)):
+                print("Correct")
+                point = "c"
+            elif ans == "":
+                point = "b"
+            else:
+                print("Wrong")
+                print(round(A-B,2))
+                point = "w"
+        else:
+            if ans == str(round(B-A,2)):
+                print("Correct")
+                point = "c"
+            elif ans =="":
+                point = "b"
+            else:
+                print("Wrong")
+                print(round(B-A,2))   
+                point = "w"
+    return point
+
+
+operations = ["+", "-", "*", "/", "*", "*"]
+response = ["yes", "y", "t", "true", "+", "q", "quit"]
+
+
+while True:
+    Quit = input("Quit? ").lower()
+    if Quit not in response: 
+        
+        T = 0
+        question = 0
+        grade = 0
+        try:
+            limit = int(input("time limit (seconds): "))+1
+        except:
+            limit = 600
+        
+        while T<limit:
+            # stage 1
+            for i in range(30):
+                question += 1
+                op = np.random.choice(operations)
+                A,B= gennumber(n1=12,dec1=False, dec2 = False)
+                ans, Elapsed = timed(A, B, op) 
+                points = check(A,B, op, ans)
+                check(A,B,op, ans)
+                if points == "c":
+                    grade += 1
+                elif points == "b":
+                    grade -= 2
+                elif points =="w":
+                    grade -= 3 
+                T+= Elapsed
+            
+            # stage 2
+            for i in range(30):
+                question += 1
+                op = np.random.choice(operations)
+                A,B= gennumber()
+                ans, Elapsed = timed(A, B, op) 
+                points = check(A,B, op, ans)
+                if points == "c":
+                    grade += 2
+                elif points == "b":
+                    grade -= 1
+                elif points =="w":
+                    grade -= 1
+                T+= Elapsed
+             
+            # stage 3
+            for i in range(15):
+                question += 1
+                op = "*"
+                A,B= gennumber(n1=1000)
+                ans, Elapsed = timed(A, B, op) 
+                points = check(A,B, op, ans)
+                check(A,B,op, ans)
+                if points == "c":
+                    grade += 2
+                elif points == "b":
+                    grade -= 1
+                elif points =="w":
+                    grade -= 2
+                T+= Elapsed
+                
+        print("\n")  
+        print("Total time (seconds): ", T)
+        print("Questions:", question)
+        print("Points: ", grade)
+        
+    else:
+        print("Total time (seconds): ", T)
+        print("Questions:", question)
+        print("Points: ", grade)
+        break
+    
