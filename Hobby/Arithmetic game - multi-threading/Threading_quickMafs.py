cf26bff3779ca749c0fac6994b4806bd8b099f3d
Shervin Rad <40181828+shervinrad100@users.noreply.github.com>
2021-05-26 23:06:25 +0100

    Update Threading_quickMafs.py

--- Hobby/Arithmetic game - multi-threading/Threading_quickMafs.py
+++ Hobby/Arithmetic game - multi-threading/Threading_quickMafs.py
@@ -1,39 +1,68 @@
 import time
-import msvcrt
-import sys
+import QuickMafs
 
-"""
-def diff(difficulty):
-    if difficulty like "hard":
-        t = 3
-    elif difficulty like "medium":
-        t = 5
-    else:
-        t = 3
-"""
+# from threading import Thread 
+import multiprocessing
+from threading import Thread 
 
 
-class TimeoutExpired(Exception):
-    pass
 
-def input_with_timeout(prompt, timeout):
-    timer = time.monotonic
-    sys.stdout.write(prompt)
-    sys.stdout.flush() 
-    endtime = timer() + timeout
-    result = []
-    while timer() < endtime:
-        if msvcrt.kbhit():    
-            result.append(msvcrt.getwche())
-            if result[-1] == "\n" or result == " ":
-                return ''.join(result[:-1])
-        time.sleep(0.04)
-    raise TimeoutExpired
+
+if __name__ == "__main__":
+    question = 0
+    score = 0
     
-try:
-    ans = input_with_timeout("2+2: \n",10)
-    print(ans)
-except TimeoutExpired:
-    print("Time's Up")
-else:
-    print(ans)
+    def timelimit():
+        time.sleep(10)
+        if correct != None:
+            return
+        print("too slow")
+
+     
+    
+    while True: 
+        # Main Menu
+        print("If you wish to quit, please enter: 'quit'")
+        print("For the score, please enter: 'score'")
+        
+        
+        print("For help, please enter: 'help'")
+        math = input('Please choose from: +, -, *, / \n')
+        
+        if math == 'quit':
+            break
+        
+        elif math == 'help':
+            print('The aim of the game is for you to practice your mental maths skills.',
+                'You can choose one of the four main operations to practice.',
+                'Two random numbers will be generated and you will need to enter your answer.')
+            
+        elif math == "score":
+            try:
+                print(f"Score: {score}/{question} ({round(score/question*100,2)}%)")
+            except ZeroDivisionError:
+                print("Score: 0/0 (0.0%)")
+
+        elif math in ["+","*","-","/"]:
+            question += 1
+            ez = input("choose difficulty: Easy (True), or hard (False)? ").lower()
+            if ez == "quit":
+                break
+            
+            elif ["He chose hard" for t in list(ez) if t == "f"]:
+                ez = False
+            else:
+                ez = True
+
+            correct = None
+            Thread(target = timelimit).start()
+            correct = QuickMafs.play(math, ez)   
+            if correct:
+                score += 1
+            
+            
+            
+        else:
+            continue
+        
+    print('Thanks for playing')
